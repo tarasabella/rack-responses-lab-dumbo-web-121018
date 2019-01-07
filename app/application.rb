989f0bc92b649a8ceb421456f5c847d4b1f0d52a
Tara Sabella <tarasabella@gmail.com>
2019-01-07 19:33:40 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+ 
+    morning = Kernel.rand(00..12)
+    noon = Kernel.rand(12..24)
+    
+ 
+    resp.write "#{morning}\n"
+    resp.write "#{noon}\n"
+  
+ 
+    if morning
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end
+ 
+    resp.finish
+  end
+ 
+end
