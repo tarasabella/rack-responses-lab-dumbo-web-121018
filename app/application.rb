8a9bf41bc6046cfa836ffe201b65f416b91cc402
Tara Sabella <tarasabella@gmail.com>
2019-01-07 19:44:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,24 +3,24 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    morning = Kernel.rand(00...12)
-    noon = Kernel.rand(12..24)
+#     morning = Kernel.rand(00...12)
+#     noon = Kernel.rand(12..24)
     
  
-    resp.write "#{morning}\n"
-    resp.write "#{noon}\n"
+#     resp.write "#{morning}\n"
+#     resp.write "#{noon}\n"
   
  
-    if morning
-      resp.write "Good Morning!"
-    else 
-      resp.write "Good Afternoon!"
-    end
+#     if morning
+#       resp.write "Good Morning!"
+#     else 
+#       resp.write "Good Afternoon!"
+#     end
     
-    resp.finish
+#     resp.finish
     
-  end 
-end 
+#   end 
+# end 
     
     time = Time.now.hour 
     
